529335c12b09927eab610b20103cca02e30836a3
Bernie White <bewhite@microsoft.com>
2019-09-18 11:55:07 +1000

    Add docker image and CI entry point

---
+++ pipeline.build.ps1
@@ -0,0 +1,53 @@
+
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory = $False)]
+    [String]$Build = '0.0.1',
+
+    [Parameter(Mandatory = $False)]
+    [String]$Configuration = 'Debug',
+
+    [Parameter(Mandatory = $False)]
+    [String]$ArtifactPath = (Join-Path -Path $PWD -ChildPath out/modules)
+)
+
+Write-Host -Object "[Pipeline] -- PWD: $PWD" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- ArtifactPath: $ArtifactPath" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- BuildNumber: $($Env:BUILD_BUILDNUMBER)" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- SourceBranch: $($Env:BUILD_SOURCEBRANCH)" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- SourceBranchName: $($Env:BUILD_SOURCEBRANCHNAME)" -ForegroundColor Green;
+
+if ($Env:SYSTEM_DEBUG -eq 'true') {
+    $VerbosePreference = 'Continue';
+}
+
+if ($Env:BUILD_SOURCEBRANCH -like '*/tags/*' -and $Env:BUILD_SOURCEBRANCHNAME -like 'v0.*') {
+    $Build = $Env:BUILD_SOURCEBRANCHNAME.Substring(1);
+}
+
+$version = $Build;
+$versionSuffix = [String]::Empty;
+
+if ($version -like '*-*') {
+    [String[]]$versionParts = $version.Split('-', [System.StringSplitOptions]::RemoveEmptyEntries);
+    $version = $versionParts[0];
+
+    if ($versionParts.Length -eq 2) {
+        $versionSuffix = $versionParts[1];
+    }
+}
+
+Write-Host -Object "[Pipeline] -- Using version: $version" -ForegroundColor Green;
+Write-Host -Object "[Pipeline] -- Using versionSuffix: $versionSuffix" -ForegroundColor Green;
+
+if ($Env:coverage -eq 'true') {
+    $CodeCoverage = $True;
+}
+
+task BuildImage {
+    docker build -f docker/stable/alpine/docker/Dockerfile -t ps-rule:latest-alpine-3.8
+}
+
+task . Build
+
+task Build BuildImage
